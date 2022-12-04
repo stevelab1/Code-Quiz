6b243307a45ab98af2c45755a40f320901e95735
stevelab1 <72598356+stevelab1@users.noreply.github.com>
2022-12-04 16:27:33 +0000

    add questions

---
+++ assets/js/logic.js
@@ -0,0 +1,8 @@
+// Grab HTML Document IDs
+var startQuiz = document.getElementById("start");
+var timer = document.getElementById("time");
+
+console.log(startQuiz);
+console.log(timer);
+startQuiz.addEventListener(onclick, timer) 
+    // timer.textContent = "3"
